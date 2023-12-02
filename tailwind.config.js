790d9622d155c899062cec6811a550173ba3f2d3
Babita1947 <130829271+Babita1947@users.noreply.github.com>
2023-12-02 13:58:44 +0530

    make responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@
 module.exports = {
   content: ["*"],
   theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '1200px',
+      // => @media (min-width: 1200px) { ... }
+
+      'lg': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      'mywidth': '765px',
+      'smw': '635px',
+      'minw': '900px',
+      'boxw' :'825px',
+      'storyw' : "1000px",
+    },
     extend: {},
   },
   plugins: [],
